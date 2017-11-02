cfe4ce16d4799c67168c45e2bb6fb76ea7ece8da
Sebastian Werner <s.werner@sebastian-software.de>
2017-11-02 17:22:47 +0100

    Initial commit with sources from readable-code

---
+++ index.js
@@ -0,0 +1,16 @@
+module.exports = {
+  extends: [
+    "./rules/atrules.yml",
+    "./rules/basics.yml",
+    "./rules/block.yml",
+    "./rules/comments.yml",
+    "./rules/declaration.yml",
+    "./rules/functions.yml",
+    "./rules/mediaquery.yml",
+    "./rules/order.yml",
+    "./rules/rules.yml",
+    "./rules/scss.yml",
+    "./rules/selector.yml",
+    "./rules/types.yml"
+  ]
+}
